81587280bd0dff717053be45832f3d43702da762
Uday Kiran Reddy <udaykiranreddymavuri@gmail.com>
2026-03-07 14:50:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import gensim
 import gensim.corpora as corpora 
-import nltk
+import nltk 
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from bs4 import BeautifulSoup
@@ -97,6 +97,7 @@ for k in [1, 5, 10]:
     print(f"Recall@{k}: {recall_at_k(final_similarity, k):.2f}")
 
 print("✔ Execution completed successfully")
+
 
 
 
